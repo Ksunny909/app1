d39d26f26c4340016cb57a885e56d3945b777a7e
Ksunny909 <kvs34@tpu.ru>
2024-04-04 11:16:13 +0700

    filter

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
 
-def catalog(request):
+def catalog(request, category_slug):
       
-	goods = Products.objects.all()
+
+	if category_slug == "all":
+		goods = Products.objects.all()
+    # elif query:
+    #     goods = q_search(query)
+	else:
+		goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
 	context = {
 		'title': 'Home - каталог',
@@ -12,5 +18,7 @@ def catalog(request):
 	}
 	return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
