b1ebcb8de6d3e53928e7ebe91e5059c179b93053
Ksunny909 <kvs34@tpu.ru>
2024-04-06 00:56:18 +0700

    cart includes

--- users/views.py
+++ users/views.py
@@ -111,8 +111,8 @@ def logout(request):
 #     return render(request, "users/profile.html", context)
 
 
-# # def users_cart(request):
-# #     return render(request, "users/users_cart.html")
+def users_cart(request):
+    return render(request, "users/users_cart.html")
 
 
 # @login_required
